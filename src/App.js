45e56e440eb73cc95550f8834da10bae604cc58d
Shashijass <shashi16ic14@gmail.com>
2021-12-06 22:38:04 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { User } from "./component/form";
+import { Third } from "./component/third";
+
+function App() {
+	return (
+		<div className="App">
+			{/* <div>working</div> */}
+			<User></User>
+		</div>
+	);
+}
+
+export default App;
